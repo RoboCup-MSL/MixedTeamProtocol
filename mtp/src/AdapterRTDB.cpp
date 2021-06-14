5447d2bc54ae8b62d433415633a23f285932df0c
Rob Burgers <rob.burgers@buxit.nl>
2021-06-14 19:22:58 +0200

    use rtdb3 method for retrieving agents

--- mtp/src/AdapterRTDB.cpp
+++ mtp/src/AdapterRTDB.cpp
@@ -23,17 +23,9 @@ AdapterRTDB::~AdapterRTDB()
 std::set<ClientType> AdapterRTDB::getClients() // TODO: const
 {
     std::set<ClientType> result;
-    // TODO: solve this properly in RTDB3 ... see also hardcoded agent limit 10 in RtDB2.cpp
-    RtDB2FrameSelection selection;
-    selection.local = true;
-    selection.shared = true;
-    RtDB2Frame frame;
-    if (getFrame(frame, selection) == RTDB2_SUCCESS)
+    for (auto item: getAgentIds())
     {
-        for (auto item: frame.items)
-        {
-            result.insert(ClientType(item.agent));
-        }
+        result.insert(ClientType(item));
     }
     return result;
 }
